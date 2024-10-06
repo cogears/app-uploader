61c544fa594c87d5a3b35bac7078cfad5c402f43
stone <stone.grandia@163.com>
2024-10-06 09:44:36 +0800

    fix sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker pull stone2grandia/app-uploader
 docker tag stone2grandia/app-uploader app-uploader
 docker rmi stone2grandia/app-uploader
-docker run --init --name uploader --restart=always -p 9527:3000 -v /opt/html/uploads:/app/uploads -d app-uploader
+docker run --name uploader --restart=always -p 9527:3000 -v /opt/html/uploads:/app/uploads -d app-uploader
